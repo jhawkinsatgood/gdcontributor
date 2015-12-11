19f29ee2bee96d96e33cb665b836441638c7ee42
jhawkinsatgood <jhawkins@good.com>
2015-12-11 12:40:19 -0800

    Update rebuild.sh
    Made it a bit more sophisticated: now only copies changed files, and never copies the plugins/ sub-directory.

--- forPhoneGap/scripts/rebuild.sh
+++ forPhoneGap/scripts/rebuild.sh
@@ -1,11 +1,26 @@
-#!/bin/sh
 
-# ToDo: Replace the following by making contributor.jhawkins a proper plugin
-rm -rf www/com.good.example.contributor.jhawkins
-cp -Rf ../../src/com.good.example.contributor.jhawkins \
-      www/com.good.example.contributor.jhawkins
+function syncWWW()
+{
+    SYNC_FROM="$1"
+    shift
+    SYNC_TO="$1"
+    shift
 
-# Script to re-prepare but without deleting GoodDynamics.js for example
-cp -Rf www/ platforms/android/assets/www/
-cp -Rf www/ platforms/ios/www/
+    # Create any directories that exist in the source, but not in the target.
+    find "$SYNC_FROM" -not -path '*/plugins/*' -type d \
+        -exec test '!' -d "$SYNC_TO/"{} \; \
+        -exec mkdir "$SYNC_TO/"{} \; \
+        -print
 
+    # Copy any files that are newer than the root. The calling code has to set
+    # the modification time of the root so that there is no wasteful copying.
+    find "$SYNC_FROM" -not -path '*/plugins/*' -type f \
+        '(' -newer "$SYNC_FROM" -or -exec test '!' -f "$SYNC_TO/"{} \; ')' \
+        -exec cp {} "$SYNC_TO/"{} \; \
+        -print
+}
+
+echo "Synchronising asset files for Android"
+syncWWW www platforms/android/assets
+echo "Synchronising asset files for iOS"
+syncWWW www platforms/ios
