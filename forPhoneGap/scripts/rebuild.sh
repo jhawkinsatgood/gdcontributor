b344aec730f8a1a8f076f2e790487a9d073e1a49
jhawkinsatgood <jhawkins@good.com>
2015-12-11 13:43:05 -0800

    Update rebuild.sh
    Added touch at the end to prevent wasteful copying. Re-indented to fit in a document elsewhere.

--- forPhoneGap/scripts/rebuild.sh
+++ forPhoneGap/scripts/rebuild.sh
@@ -6,16 +6,19 @@ function syncWWW()
     SYNC_TO="$1"
     shift
 
-    # Create any directories that exist in the source, but not in the target.
+    # Create any directories that exist in the source, but not in the
+    # target.
     find "$SYNC_FROM" -not -path '*/plugins/*' -type d \
         -exec test '!' -d "$SYNC_TO/"{} \; \
         -exec mkdir "$SYNC_TO/"{} \; \
         -print
 
-    # Copy any files that are newer than the root. The calling code has to set
-    # the modification time of the root so that there is no wasteful copying.
+    # Copy any files that are newer than the root. The calling code has to
+    # set the modification time of the root so that there is no wasteful
+    # copying.
     find "$SYNC_FROM" -not -path '*/plugins/*' -type f \
-        '(' -newer "$SYNC_FROM" -or -exec test '!' -f "$SYNC_TO/"{} \; ')' \
+        '(' -newer "$SYNC_FROM" -or \
+            -exec test '!' -f "$SYNC_TO/"{} \; ')' \
         -exec cp {} "$SYNC_TO/"{} \; \
         -print
 }
@@ -24,3 +27,4 @@ echo "Synchronising asset files for Android"
 syncWWW www platforms/android/assets
 echo "Synchronising asset files for iOS"
 syncWWW www platforms/ios
+touch www
